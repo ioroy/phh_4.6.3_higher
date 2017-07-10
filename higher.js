0ce860ab9d3fd74a38daecd19dd572202b0de496
ioroy <iori0130pad@gmail.com>
2017-07-10 16:00:54 +0900

    高階関数の活用

--- higher.js
+++ higher.js
@@ -5,8 +5,13 @@ function arrayWalk (data, f) {
 }
 
 function showElement (value, key) {
-    console.log(key + ' : ' + value);
+    console.log('キー（'　+ key + ')' + ' : '　 +'値（' + value + ')');
 }
+
+function showDecoratedElement (value, key) {
+    console.log('キー（'　+ key + ')' + ' : '　 +'値（' + value + ')');
+}
+
 var ary = [1, 2, 4, 8, 16];
 
 arrayWalk (ary, showElement);
